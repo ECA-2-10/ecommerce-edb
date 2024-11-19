b85a7ec4d4e4bcfaaeecf557f3899eb02934a448
jose-maria-portela <92798583+jose-maria-portela@users.noreply.github.com>
2024-11-20 00:38:39 +0100

    added the products and categories, and manual in readme

--- product/models.py
+++ product/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
+
 # Create your models here.
-class Product(models.Model):
-    id = models.BigAutoField(primary_key=True, unique=True, auto_created=True)
-    name = models.CharField(max_length=125)
-    description = models.CharField(max_length=500)
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)], help_text='El precio debe ser un valor positivo.')
-    image = models.ImageField(upload_to='products', verbose_name='Imagen')
-    maker = models.CharField(max_length=255)
-    stock = models.IntegerField(validators=[MinValueValidator(0)], help_text='El stock debe ser igual o mayor que cero.')
+class Category(models.Model):
     department = models.CharField(max_length=255)
     section = models.CharField(max_length=255)
 
-    # Comprobar que va bien este formato para mostrar el nombre
     def __str__(self):
-        return f'Producto: {self.name}'
+        return f'{self.department}, {self.section}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=125)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)], help_text='El precio debe ser un valor positivo.')
+    image = models.ImageField(upload_to='product/images', verbose_name='Imagen')
+    maker = models.CharField(max_length=255)
+    stock = models.IntegerField(validators=[MinValueValidator(0)], help_text='El stock debe ser igual o mayor que cero.')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return f'{self.name} ({self.maker})'
