a0bac9862f810d02d859386b3ad814328fc31357
Cargarort3 <cargarort3@alum.us.es>
2024-11-20 12:34:42 +0100

    fix account

--- account/views.py
+++ account/views.py
@@ -6,43 +6,41 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib import messages
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginForm
 
 def register_view(request):
+    if request.user.is_authenticated:
+            return redirect('/')
+
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Cuenta creada con éxito.')
             return redirect('login')
-        else:
-            messages.error(request, 'Hubo un error al crear la cuenta.')
+
     else:
         form = CustomUserCreationForm()
     
     return render(request, 'account/register.html', {'form': form})
 
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib import messages
-from .forms import LoginForm
-
-
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = authenticate(request, username=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            messages.error(request, "¡Nombre de usuario o contraseña inválidos!")
-            return redirect('login')
-
-    return render(request, 'account/login.html')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'account/login.html', {'form': form})
 
 
 def logout_view(request):
