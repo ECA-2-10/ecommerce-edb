7c552653c7af56f24204574a68f0d8d7b2e08e15
Cargarort3 <cargarort3@alum.us.es>
2024-11-20 12:35:33 +0100

    add order relations

--- order/models.py
+++ order/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError  
 from django.utils.timezone import now 
+from account.models import CustomUser
+from product.models import Product
 
 def validate_delivery_date(value):
-    if value < now():
+    if value < now().date():
         raise ValidationError("The delivery date cannot be in the past.")
 
 # Create your models here.
@@ -29,7 +31,21 @@ class Order(models.Model):
         choices=PAYMENT_METHODS,
         default=ON_DELIVERY
     )
-    deliveryDate = models.DateTimeField(validators=[validate_delivery_date])
+    deliveryDate = models.DateField(validators=[validate_delivery_date])
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
 
     def __str__(self):
         return f'Pedido {self.id} - {self.email}'
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_products')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_products')
+    quantity = models.PositiveIntegerField(
+        default=1,
+        validators=[MinValueValidator(1)],
+        help_text='La cantidad debe ser al menos 1.'
+    )
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name} en {self.order}'
